35a393d128c393cc1e472b219dd7d18b7f962c21
bbcoyote <huffmancharles14@gmail.com>
2022-08-31 15:13:40 -0500

    just deleted the ===== and commented out the 2nd nav feature to make it work. Need to switch to main branch and do git pull

--- client/src/components/navbar/navbar.component.jsx
+++ client/src/components/navbar/navbar.component.jsx
@@ -43,29 +43,29 @@ useEffect(() => {
   );
 };
 
-export default Navbar;
-=======
-import React from 'react';
-import './navbar.styles.css';
+ export default Navbar;
 
-function Navbar () {
+// import React from 'react';
+// import './navbar.styles.css';
 
-        return(
-            <div className='Navbar'>
-                <div className='leftSide'>
-                    </div>
-                    <div className='middle'>
-                        <input type='search' placeholder='Search App'/>
-                    </div>
-                    <div className='rightSide'>
-                        <button className='logIn'>Log In</button>
-                        <button className='signUp'>Sign Up</button>
-                    </div>
-                </div>
+// function Navbar () {
+
+//         return(
+//             <div className='Navbar'>
+//                 <div className='leftSide'>
+//                     </div>
+//                     <div className='middle'>
+//                         <input type='search' placeholder='Search App'/>
+//                     </div>
+//                     <div className='rightSide'>
+//                         <button className='logIn'>Log In</button>
+//                         <button className='signUp'>Sign Up</button>
+//                     </div>
+//                 </div>
 
 
         
-            )
-    }
+//             )
+//     }
 
-export default Navbar;
+// export default Navbar;
